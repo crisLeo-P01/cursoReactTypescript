ced18882e681ef32446ac01295b127d54b5ef182
crisleo-P01 <cristianleopaiva@gmail.com>
2025-10-15 19:10:17 -0300

    Agregado función para los botones de restar o sumar productos. Además también como es el manejo con CSS y trabajar con CSS module

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,26 +1,34 @@
+import { useState } from "react";
+
+import './ItemCounter.css'
+import styles from './ItemCounter.module.css'
+
 interface Props {
     name: string;
     quantity?: number
 }
 
-export const ItemCounter = ({name, quantity}: Props) => {
+export const ItemCounter = ({ name, quantity = 1 }: Props) => {
+    const [ count, setCount ] = useState(quantity)
+
+    const handleSubtract = () => {
+        if(count === 1) return;
+
+        return setCount(count - 1)
+    }
+
+    const handleAdd = () => {
+        return setCount(count + 1)
+    }
+
     return (
-        <section style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: 10,
-            marginTop: 10
-        }}>
-            <span style={{
-                width: 150
-            }}>{name}</span>
-            <button
-                onClick={() => {
-                    console.log(`clic ${name}`)
-                }}
-            >-1</button>
-            <span>{quantity}</span>
-            <button>+1</button>
+        <section className="item-row">
+            <span className="item-text" style={{
+                color: count === 1 ? 'red' : 'black'
+            }}>{ name }</span>
+            <button className={ styles.padding } onClick={ handleSubtract }>-1</button>
+            <span className={ styles.fontSize }>{ count }</span>
+            <button className={ styles.padding } onClick={ handleAdd }>+1</button>
         </section>
     )
 }
